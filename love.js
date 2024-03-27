a1c1b87e9e066a1315a8319a0b6ae0fc5eb643bd
SalonJee <148306222+SalonJee@users.noreply.github.com>
2024-03-27 13:09:09 +0545

    Add files via upload

---
+++ love.js
@@ -0,0 +1,37 @@
+
+
+let name1= document.getElementById('name1');
+let name2= document.getElementById('name2');
+let score= document.getElementById('score');
+
+
+function init(){
+ 
+   let result = calculateLoveBetween(name1.ariaValueMax, name2.value)
+   score.innerHTML = result + "%";
+
+   const jsConfetti = new JSConfetti()
+   jsConfetti.addConfetti({
+    emojis: ['❤️'],
+    emojiSize: 50,
+    confettiNumber: 100,
+   })
+
+}
+function calculateLoveBetween(name1,name2){
+    let combineNames = name1+name2 ;
+    let uniqNames= [... new Set(combineNames.toLowerCase
+        ())]
+       
+        let total = 0;
+
+        for(var char of uniqNames){
+            total += char.charCodeAt();
+        }
+
+        let loverPercentage=(total % 100)+1;
+     return loverPercentage;
+}
+
+
+
